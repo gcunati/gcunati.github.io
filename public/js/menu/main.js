d0b00195b52a236e38f1bd91b632ad9bc5256e9c
Gastón Cunati <gastoncunatiecon@gmail.com>
2022-02-12 16:01:11 -0300

    ¿Corrección de redireccionamiento?

--- public/js/menu/main.js
+++ public/js/menu/main.js
@@ -30,26 +30,21 @@ function marcarPlantilla(id) {
 // Llamado a ejecutar scripts de la plantilla inyectada
 
 function initJs(id) {
-    if(window.location.href == "https://gcunati.github.io/"){
-        location.href = "https://connecticus-ecommerce.glitch.me/#inicio"
-    }
-    else{
-        switch(id) {
-            case 'inicio':
-                initInicio()
-                break
-            case 'alta':
-                initAlta()
-                break
-            case 'contacto':
-                break
-            case 'nosotros':
-                break
-            default:
-                initInicio()
-        }
-    }
 
+    switch(id) {
+        case 'inicio':
+            initInicio()
+            break
+        case 'alta':
+            initAlta()
+            break
+        case 'contacto':
+            break
+        case 'nosotros':
+            break
+        default:
+            initInicio()
+    }
 }
 
 
